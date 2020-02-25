e383cc81b60ced935889fc8e62f8667f8e2675da
paras594 <parasarora594@gmail.com>
2020-02-25 18:00:59 +0530

    testing environment variables at netlify

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import TopicsNav from "./components/TopicsNav";
 import Homepage from "./routes/Homepage";
 
 const App = () => {
+	console.log(process.env.API_KEY);
 	return (
 		<Container>
 			<Navbar />
